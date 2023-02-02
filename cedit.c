8fe9a31051ae9bc25cea146b05917a40551675b7
Aniket <singhaniket@tutanota.com>
2023-02-02 08:05:53 +0530

    full implementation of RawMode

--- cedit.c
+++ cedit.c
@@ -27,11 +27,27 @@ void enableRawMode() {
   // to be called at exit.
 
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON);
-  // c_lflag is a member of the termios structure that contains local modes.
+
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ICRNL | IXON);
+  // Bitwise not of BRKINT disables the SIGINT signal when the user presses
+  // Ctrl-C. ICRNL disables the carriage return to new line translation. INPCK
+  // disables parity checking. Icrnl is a flag that converts carriage return to
+  // newline on input. Bitwise not of IXON disables software flow control(ctl-s
+  // and ctrl-q).
+  raw.c_oflag &= ~(OPOST);
+  // Bitwise not of OPOST disables output processing.
+  raw.c_cflag |= (CS8);
+  // Bitwise or of CS8 sets character size to 8 bits per byte.
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
   // Bit wise of ECHO stops the terminal from echoing the input and ICANON stops
-  // the terminal from waiting for the user to press enter before reading the
-  // input.
+  // the terminal  from waiting for the user to press enter before reading the
+  // input. ISIG  stops the terminal from sending signals like ctrl-c and
+  // ctrl-z. IEXTEN  stops the terminal from sending ctrl-v.
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+  // VMIN sets the minimum number of bytes of input needed for read to return
+  // VTIME sets the maximum amount of time to wait before read returns.
+
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
   // tcsetattr() sets the parameters associated with the terminal.
   // TCSAFLUSH is a flag that specifies when to apply the change.
@@ -43,19 +59,20 @@ void enableRawMode() {
 int main() {
   enableRawMode();
 
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
-  // read() reads up to 1 byte from the file descriptor STDIN_FILENO(which is
-  // standard input output) into the buffer starting at &c and if c reads 'q'
-  // program terminates.
-  {
+  while (1) {
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
+    // read() reads up to n bytes from the standard input into the buffer
     if (iscntrl(c)) {
-      printf("%d\n", c);
+      printf("%d\r\n", c);
     } else {
-      printf("%d ('%c')\n", c, c);
+      printf("%d ('%c')\r\n", c, c);
     }
     // if character is a control character then print its ASCII value else print
     // its ASCII value and the character itself.
+    if (c == 'q')
+      break;
+    // if the character is q then break out of the loop.
   };
   return 0;
 }
