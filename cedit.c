7558cb2741afb21876e046ae50f9257e22ddcf74
Aniket <singhaniket@tutanota.com>
2023-02-04 04:54:30 +0530

    add window rendering
    render ~ on left side, also capable of finding height and width of window

--- cedit.c
+++ cedit.c
@@ -2,8 +2,12 @@
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
+#include <sys/ioctl.h>
+// ioctl provides terminal size
 #include <stdlib.h>
 // for the atexit() function
+#include <string.h>
+// for functions related to buffer handling
 #include <termios.h>
 // termios.h is a header file that provides access to the POSIX terminal
 // interface. These being different input and output modes.
@@ -12,8 +16,14 @@
 // API.
 
 /* data */
-struct termios orig_termios;
-// This struct will store the original terminal attributes.
+struct editorConfig {
+  // This is a structure that contains the editor configuration.
+  int screenrows;
+  int screencols;
+  struct termios orig_termios;
+  // This is a structure that contains the original terminal attributes.
+};
+struct editorConfig E;
 
 /* defines */
 #define CTRL_KEY(k) ((k)&0x1f)
@@ -21,20 +31,24 @@ struct termios orig_termios;
 
 /* terminal */
 void die(const char *s) {
-  // This function will print the error message and exit the program.
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+  // Clear the screen and move the cursor to the top left corner before exiting
+
   perror(s);
+  // print the error message and exit the program.
   exit(1);
 }
 
 void disableRawMode() {
   // This function will restore the original terminal attributes.
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
     die("tcsetattr");
 }
 
 void enableRawMode() {
 
-  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
     die("tcgetattr");
   // tcgetattr() gets the parameters associated with the terminal and stores
   // them in the termios structure.
@@ -42,7 +56,7 @@ void enableRawMode() {
   // atexit() is a stdlib function which registers the function disableRawMode()
   // to be called at exit.
 
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
 
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ICRNL | IXON);
   // Bitwise not of BRKINT disables the SIGINT signal when the user presses
@@ -84,12 +98,44 @@ char editorReadKey() {
   return c;
 }
 
+int getWindowSize(int *rows, int *cols) {
+  // This function will get the size of the terminal window.
+  struct winsize ws;
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    // on sucess ioctl() returns 0 and put the size of the terminal in ws.
+    return -1;
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
 /* Output functions */
+void editorDrawRows() {
+  // This function will draw the rows of the editor.
+  int y;
+  for (y = 0; y < E.screenrows; y++) {
+
+    if (y < E.screenrows - 1) {
+      // if we are not at the last row, move the cursor to the next line.
+      write(STDOUT_FILENO, "\r\n", 2);
+    }
+  }
+}
+
 void editorRefreshScreen() {
   // This function will clear the screen and draw the welcome message.
   write(STDOUT_FILENO, "\x1b[2J", 4);
   // \x1b is the escape character. [2J is the escape sequence that clears the
   // screen.
+  write(STDOUT_FILENO, "\x1b[H", 3);
+  // \x1b is the escape character. [H is the escape sequence that moves the
+  // cursor to the top left corner of the screen.
+  editorDrawRows();
+  write(STDOUT_FILENO, "\x1b[H", 3);
+  // \x1b is the escape character. [H is the escape sequence that moves the
+  // cursor to the top left corner of the screen.
 }
 
 /* input functions */
@@ -98,14 +144,25 @@ void editorProcessKeypress() {
   char c = editorReadKey();
   switch (c) {
   case CTRL_KEY('q'):
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+    // Clear the screen before exiting
+
     exit(0);
     break;
   }
 }
 
 /* Code Initialsation */
+void initEditor() {
+  // This function will initialise the editor.
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1)
+    die("getWindowSize");
+}
+
 int main() {
   enableRawMode();
+  initEditor();
 
   while (1) {
     editorRefreshScreen();
